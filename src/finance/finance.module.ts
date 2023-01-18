b40dadef5f1fb5c52de18fdd5804bbc21bb6452d
Rozi Putra <rozi@nusa.net.id>
2023-01-18 13:18:14 +0700

    IS-710 remove financeController declaration

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FinanceService } from './finance.service';
-import { FinanceController } from './finance.controller';
 import { StockInvoiceRepository } from './repositories/stock-invoice-repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { StockInvoice } from './entities/stock-invoice.entity';
 
 @Module({
-  controllers: [FinanceController],
   providers: [FinanceService, StockInvoiceRepository],
 })
 export class FinanceModule {}
