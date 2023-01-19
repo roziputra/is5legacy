80bc69aa725477797d5404fe5034ee1771e7b0c1
Rozi Putra <rozi@nusa.net.id>
2023-01-19 21:07:39 +0700

    IS-496 cron perhitungan penyusutan

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TtsModule } from 'src/tickets/tickets.module';
+import { FinanceModule } from 'src/finance/finance.module';
+import { CronService } from './cron.service';
+import { GeneralTicketExpiredDocService } from './general-ticket-expired-doc.service';
+import { DepreciationJournalService } from './depreciation-journal.service';
 
 @Module({
-  imports: [TtsModule],
+  imports: [TtsModule, FinanceModule],
+  providers: [
+    CronService,
+    GeneralTicketExpiredDocService,
+    DepreciationJournalService,
+  ],
 })
 export class CronModule {}
