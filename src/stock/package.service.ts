cde11d89fc49ddcde9707ac9c5e4ba0e0c085b3b
Rozi Putra <rozi@nusa.net.id>
2023-03-23 16:08:53 +0700

    IS-1512 Fix list package butuh list barang

--- src/stock/package.service.ts
+++ src/stock/package.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { RequestStbPackageRepository } from './repositories/request-stb-package.repository';
 import { RequestStbPackage } from './entities/request-stb-package.entity';
 import { RequestStbPackageDetailRepository } from './repositories/request-stb-package-detail.repository';
+import { StbEngineerService } from './stb-engineer.service';
 
 @Injectable()
 export class PackageService {
   constructor(
+    private readonly stbEngineerService: StbEngineerService,
     private readonly requestStbPackageRepository: RequestStbPackageRepository,
     private readonly requestStbPackageDetailRepository: RequestStbPackageDetailRepository,
   ) {}
 
-  findPackages(): Promise<RequestStbPackage[]> {
-    return this.requestStbPackageRepository.findAllPackage();
+  findPackages(user): Promise<RequestStbPackage[]> {
+    const branch = this.stbEngineerService.getMasterBranch(user);
+    return this.requestStbPackageRepository.findAllPackage(branch);
   }
 
   findPackageDetails(id: number, branch: string) {
